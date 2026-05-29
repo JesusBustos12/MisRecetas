8c2e3f8cd0e323c427ec6b390cc9dd1b104a0281
Jesus Bustos <jesusbustosarizmendi0@gmail.com>
2026-05-28 19:11:57 -0600

    fix: refinar filtros de aside para usar solo nombres de proteinas literales y evitar platos ambiguos

--- backend/src/server.js
+++ backend/src/server.js
@@ -224,10 +224,10 @@ app.get('/api/recipes', async (req, res) => {
     }
 
     const applyTypeFilter = (q, p, typeVal) => {
-      // Regex for filtering based on content to bypass contaminated category_type
-      const meatLand = 'pollo|carne|cerdo|beef|pork|cordero|steak|ternera|chuleta|tocino|bacon|jamon|jamón|salchicha|pepperoni|salami|pavo|turkey|duck|pato|meatball|albóndiga|brisket|wings|prosciutto|guanciale|pancetta|chorizo|carnitas|cochinita|pastrami|veal|lamb|ribs|costillas|ossobuco|bolognese|boloñesa|katsu|tonkotsu|sukiyaki|okonomiyaki|gyoza|omurice|tamales|pozole|menudo|barbacoa|hot dog|perrito|hamburguesa|burger|bistecca|fiorentina|meatloaf|wonton|jambalaya|ahogada|cocido|fabada|mapo tofu|jiaozi|dumpling|dim sum|mole poblano|nogada|gravy|carbonara|ragu|ragú|cochinillo|coq|boeuf|bourguignon|cassoulet|rogan josh|moussaka|tlayuda|shabu|pastitsio|escargot|brodo';
-      const meatSea = 'pescado|fish|camaron|camarón|shrimp|marisco|seafood|salmon|salmón|atun|atún|pulpo|octopus|calamar|squid|bacalao|tuna|lobster|langosta|crab|cangrejo|mussels|mejillones|clams|almejas|ostras|oysters|vieiras|scallops|prawns|gambas|langostinos|aguachile|ceviche|takoyaki|unagi|paella|anchoa|anchovy|pissaladiere|pissaladière|chowder|bouillabaisse|nicoise|niçoise|chawanmushi|coquilles|saint-jacques';
-      const dessertTerms = 'postre|dulce|tarta|pastel|cake|dessert|chocolate|helado|flan|galleta|cookie|brownie|muffin|cupcake|mermelada|mousse|creme brulee|pudding|pudin|pay|pie|caramelo|tiramisu|tiramisú|gelato|cannoli|panettone|panna cotta|crepe|crêpe|macaron|baklava|dorayaki|mochi|profiteroles|churros|sfogliatella|zabaione|zeppole|loukoumades|mooncakes';
+      // Regex estricto con proteínas reales (sin nombres de platos ambiguos como 'tamales' o 'gyoza')
+      const meatLand = 'pollo|chicken|carne|cerdo|beef|pork|cordero|steak|ternera|chuleta|tocino|bacon|jamon|jamón|ham|salchicha|sausage|pepperoni|salami|pavo|turkey|duck|pato|meatball|albóndiga|brisket|wings|prosciutto|guanciale|pancetta|chorizo|carnitas|cochinita|pastrami|veal|lamb|ribs|costillas|ossobuco|bolognese|boloñesa|tonkotsu|menudo|barbacoa|hot dog|perrito|hamburguesa|burger|bistecca|meatloaf|cochinillo|coq|boeuf|escargot';
+      const meatSea = 'pescado|fish|camaron|camarón|shrimp|marisco|seafood|salmon|salmón|atun|atún|pulpo|octopus|calamar|squid|bacalao|cod\\\\b|tuna|lobster|langosta|crab|cangrejo|mussels|mejillones|clams|almejas|ostras|oysters|vieiras|scallops|prawns|gambas|langostinos|aguachile|ceviche|unagi|anchoa|anchovy';
+      const dessertTerms = 'postre|dulce|tarta|pastel|cake|dessert|chocolate|helado|flan|galleta|cookie|brownie|muffin|cupcake|mermelada|mousse|creme brulee|pudding|pudin|pay|pie|caramelo|tiramisu|tiramisú|gelato|cannoli|panettone|panna cotta|crepe|crêpe|macaron|baklava|dorayaki|mochi|profiteroles|churros|sfogliatella|zabaione|zeppole|loukoumades|mooncakes|sweet|sugar|azucar|vainilla|vanilla';
       
       const type = typeVal ? typeVal.toLowerCase() : '';
       
