a08d6be2ffdbc02f5eb2a720becf110b6a1e8374
Jesus Bustos <jesusbustosarizmendi0@gmail.com>
2026-05-28 19:15:21 -0600

    fix: refinar filtro de mariscos para evitar falsos positivos con ceviches veganos y la fruta tuna

--- backend/src/server.js
+++ backend/src/server.js
@@ -226,7 +226,7 @@ app.get('/api/recipes', async (req, res) => {
     const applyTypeFilter = (q, p, typeVal) => {
       // Regex estricto con proteínas reales (sin nombres de platos ambiguos como 'tamales' o 'gyoza')
       const meatLand = 'pollo|chicken|carne|cerdo|beef|pork|cordero|steak|ternera|chuleta|tocino|bacon|jamon|jamón|ham|salchicha|sausage|pepperoni|salami|pavo|turkey|duck|pato|meatball|albóndiga|brisket|wings|prosciutto|guanciale|pancetta|chorizo|carnitas|cochinita|pastrami|veal|lamb|ribs|costillas|ossobuco|bolognese|boloñesa|tonkotsu|menudo|barbacoa|hot dog|perrito|hamburguesa|burger|bistecca|meatloaf|cochinillo|coq|boeuf|escargot';
-      const meatSea = 'pescado|fish|camaron|camarón|shrimp|marisco|seafood|salmon|salmón|atun|atún|pulpo|octopus|calamar|squid|bacalao|cod\\\\b|tuna|lobster|langosta|crab|cangrejo|mussels|mejillones|clams|almejas|ostras|oysters|vieiras|scallops|prawns|gambas|langostinos|aguachile|ceviche|unagi|anchoa|anchovy';
+      const meatSea = 'pescado|fish|camaron|camarón|shrimp|marisco|seafood|salmon|salmón|atun|atún|pulpo|octopus|calamar|squid|bacalao|lobster|langosta|crab|cangrejo|mussels|mejillones|clams|almejas|ostras|oysters|vieiras|scallops|prawns|gambas|langostinos|unagi|anchoa|anchovy';
       const dessertTerms = 'postre|dulce|tarta|pastel|cake|dessert|chocolate|helado|flan|galleta|cookie|brownie|muffin|cupcake|mermelada|mousse|creme brulee|pudding|pudin|pay|pie|caramelo|tiramisu|tiramisú|gelato|cannoli|panettone|panna cotta|crepe|crêpe|macaron|baklava|dorayaki|mochi|profiteroles|churros|sfogliatella|zabaione|zeppole|loukoumades|mooncakes|sweet|sugar|azucar|vainilla|vanilla';
       
       const type = typeVal ? typeVal.toLowerCase() : '';
